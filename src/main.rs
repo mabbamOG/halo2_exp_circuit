ca0089af82a1ecca9ca4f7012352aa8eb8d10926
Mario A. Barbara <mariobarbara@protonmail.ch>
2024-01-26 08:41:35 +0000

    compiles

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 
-use eth_types::Field;
+#![allow(clippy::needless_range_loop)]
+#![allow(clippy::vec_init_then_push)]
+use ff::{Field, PrimeFieldBits};
+use halo2_proofs::halo2curves::ff::PrimeField;
+use halo2_proofs::plonk::{Column, Advice, Selector, ConstraintSystem,Circuit, Error, Expression};
+use halo2_proofs::circuit::{Layouter,SimpleFloorPlanner,Value};
+use halo2_proofs::poly::Rotation;
 
 #[derive(Clone, Debug)]
 pub struct ExpCircuitConfig {
@@ -8,7 +14,7 @@ pub struct ExpCircuitConfig {
     q_last: Selector, // active on row 257
     running_result: Column<Advice>, // row 0 is 1, rows 1..256 are prod(x^(2^i)^e_(i-1)), row 257 is input result
     running_base_powers: Column<Advice>, // row 0 is any, row 1 is input base, rows 2..257 are x^(2^i)
-    running_exponent: Column<Advice> // rows 0..255 are are the reverse elements of recursion e_next=e_prev*2+e_(255-i), row 256 is 0, row 257 is input exponent
+    running_exponent: Column<Advice>, // rows 0..255 are are the reverse elements of recursion e_next=e_prev*2+e_(255-i), row 256 is 0, row 257 is input exponent
     exponent_bits: Column<Advice>, // row 0 is any, rows 1..256 are e_i
 }
 
@@ -19,7 +25,7 @@ pub struct ExpCircuit<F>{
     result: F,
 }
 
-impl<F:FieldExt> Circuit<F> for ExpCircuit<F> {
+impl<F:PrimeField> Circuit<F> for ExpCircuit<F> {
     type Config = ExpCircuitConfig;
     type FloorPlanner = SimpleFloorPlanner;
 
@@ -38,11 +44,13 @@ impl<F:FieldExt> Circuit<F> for ExpCircuit<F> {
 
         meta.create_gate("INITIAL VALUES", |meta| { 
             let q_first = meta.query_selector(q_first);
-            let result_init = meta.query_advice(running_result, Rotatin::cur());
+            let result_init = meta.query_advice(running_result, Rotation::cur());
             let exponent_init = meta.query_advice(running_exponent, Rotation(256));
+            let one = Expression::Constant(F::ONE);
+            let zero = Expression::Constant(F::ZERO);
             let mut gate = vec![];
-            gate.push(("check running result default case", q_first * (result_init - 1)));
-            gate.push(("check running exponent default case", q_first * (exponent_init - 0)));
+            gate.push(("check running result default case", q_first.clone() * (result_init - one)));
+            gate.push(("check running exponent default case", q_first * (exponent_init - zero)));
             gate
         });
 
@@ -50,10 +58,10 @@ impl<F:FieldExt> Circuit<F> for ExpCircuit<F> {
             let q_last = meta.query_selector(q_last);
             let result_input = meta.query_advice(running_result, Rotation::cur());
             let result_output = meta.query_advice(running_result, Rotation::prev());
-            let exponent_input = metaa.query_advice(running_exponent, Rotatin::cur());
+            let exponent_input = meta.query_advice(running_exponent, Rotation::cur());
             let exponent_output = meta.query_advice(running_exponent, Rotation(-257));
             let mut gate = vec![];
-            gate.push(("check running result output", q_last * (result_input - result_output)));
+            gate.push(("check running result output", q_last.clone() * (result_input - result_output)));
             gate.push(("check running exponent output", q_last * (exponent_input - exponent_output)));
             gate
         });
@@ -68,15 +76,17 @@ impl<F:FieldExt> Circuit<F> for ExpCircuit<F> {
             let x2i_next = meta.query_advice(running_base_powers, Rotation::next());
             let e_next = meta.query_advice(running_exponent, Rotation::prev());
             let e_prev = meta.query_advice(running_exponent, Rotation::cur());
+            let one = Expression::Constant(F::ONE);
+            let two = Expression::Constant(F::from(2));
             let mut gate = vec![];
-            gate.push(("check running result step case", q_running * (result_next - result_prev * (x2i * ei + (1 - ei)))));
-            gate.push(("check running base powers step case", q_running * (x2i_next - x2i_prev * x2i_prev)));
-            gate.push(("check running exponent step case", q_running * (e_next - (ei + 2 * e_prev)));
-            gate.push(("check exponent bits are bits", q_running * ei * (1 - ei)))
+            gate.push(("check running result step case", q_running.clone() * (result_next - result_prev * (x2i * ei.clone() + (one.clone() - ei.clone())))));
+            gate.push(("check running base powers step case", q_running.clone() * (x2i_next - x2i_prev.clone() * x2i_prev)));
+            gate.push(("check running exponent step case", q_running.clone() * (e_next - (ei.clone() + two * e_prev))));
+            gate.push(("check exponent bits are bits", q_running * ei.clone() * (one - ei)));
             gate
         });
 
-        BlobKZGConfig {
+        ExpCircuitConfig {
             q_running,
             q_first,
             q_last,
@@ -88,21 +98,8 @@ impl<F:FieldExt> Circuit<F> for ExpCircuit<F> {
     }
 
     fn synthesize(&self, config: Self::Config, mut layouter: impl Layouter<F>) -> Result<(), Error> {
-        let running_result: [F;257] = {
-            let mut running_result = [F::default(); 257];
-            running_result[0] = 1;
-            for i in 1..=256 {
-                if exponent_bits[i-1]==1 {
-                    running_result[i] = running_result[i-1] * running_base_powers[i]
-                } else {
-                    running_result[i] = running_result[i-1];
-                }
-            }
-            running_result
-        };
-
         let running_base_powers: [F; 257] = {
-            let mut runnng_base_powers = [F::default(); 257];
+            let mut running_base_powers = [F::default(); 257];
             running_base_powers[0] = self.base;
             for i in 1..=256 {
                 running_base_powers[i] = running_base_powers[i-1] * running_base_powers[i-1];
@@ -116,20 +113,33 @@ impl<F:FieldExt> Circuit<F> for ExpCircuit<F> {
             for j in 0..32 {
                 for i in 0..8 {
                     if ((exponent_bytes[j]>>i) & 1) == 1 {
-                        exponent_bits[j*8+i] = F::one();
+                        exponent_bits[j*8+i] = F::ONE;
                     } else {
-                        exponent_bits[j*8+i] = F::zero();
+                        exponent_bits[j*8+i] = F::ZERO;
                     }
                 }
             }
             exponent_bits
         };
 
-        let running_exponent: [F;257] = {
-            let mut running_exponent = [F::default; 257];
-            running_exponent[0] = 0;
+        let running_result: [F;257] = {
+            let mut running_result = [F::default(); 257];
+            running_result[0] = F::ONE;
             for i in 1..=256 {
-                running_exponent[i] = running_exponent[i-1]*2 + exponent_bits[256-i];
+                if exponent_bits[i-1]==F::ONE {
+                    running_result[i] = running_result[i-1] * running_base_powers[i]
+                } else {
+                    running_result[i] = running_result[i-1];
+                }
+            }
+            running_result
+        };
+
+        let running_exponent: [F;257] = {
+            let mut running_exponent = [F::default(); 257];
+            running_exponent[0] = F::ZERO;
+            for i in 1..=256 {
+                running_exponent[i] = running_exponent[i-1]*F::from(2) + exponent_bits[256-i];
             }
             running_exponent
         };
@@ -148,24 +158,24 @@ impl<F:FieldExt> Circuit<F> for ExpCircuit<F> {
 
             // COLUMN: running_result
             for i in 0..=256 {
-                region.assign_advice(|| "running result", config.running_result, i, || Value::known(running_result[i]));
+                region.assign_advice(|| "running result", config.running_result, i, || Value::known(running_result[i]))?;
             }
-            regin.assign_advice(|| "input result", config.running_result, 257, || Value::known(self.result));
+            region.assign_advice(|| "input result", config.running_result, 257, || Value::known(self.result))?;
 
             // COLUMN: running_base_powers
             for i in 1..=257 {
-                region.assign_advice(|| "running base input and powers", config.running_base_powers, i, || Value::known(running_base_powers[i-1]));
+                region.assign_advice(|| "running base input and powers", config.running_base_powers, i, || Value::known(running_base_powers[i-1]))?;
             }
 
             // COLUMN: running_exponent
             for i in 0..=256 {
-                region.assign_advice(|| "reverse running exponent values", config.running_exponent, i, || Value::known(running_exponent[256-i]));
+                region.assign_advice(|| "reverse running exponent values", config.running_exponent, i, || Value::known(running_exponent[256-i]))?;
             }
-            region.assign_advice(|| "input exponent", config.running_exponent, 257, || Value::known(self.exponent));
+            region.assign_advice(|| "input exponent", config.running_exponent, 257, || Value::known(self.exponent))?;
             
             // COLUMN: exponent_bits
             for i in 1..=256 {
-                region.assign_advice(|| "exponent bits", config.exponent_bits, i, || Value::known(exponent_bits[i-1]));
+                region.assign_advice(|| "exponent bits", config.exponent_bits, i, || Value::known(exponent_bits[i-1]))?;
             }
 
             region.name_column(|| "running_result", config.running_result);
@@ -180,10 +190,12 @@ impl<F:FieldExt> Circuit<F> for ExpCircuit<F> {
 
 fn main() {
     type F = halo2_proofs::halo2curves::bn256::Fr; // Native Field
-    let (base, exponent, result) = (F::from(5), F::from(6), base.pow(exponent));
+    let (base, exponent) = (F::from(5), F::from(6));
+    let result = base.pow(exponent.to_le_bits().data);
+
     println!("base: {base:?}, exponent: {exponent:?}, result: {result:?}, MODULUS: {}", F::MODULUS);
     let circuit = ExpCircuit::<F>{base, exponent, result};
-    let prover = hal2_proofs::dev::MockProver::<N>::run(10, &circuit, vec![]).unwrap();
+    let prover = halo2_proofs::dev::MockProver::<F>::run(10, &circuit, vec![]).unwrap();
     prover.assert_satisfied();
     println!("CIRCUIT TEST COMPLETED")
 }
